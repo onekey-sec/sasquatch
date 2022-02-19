dd212a619c98ed1b652dcedf9b4b9a701a343f95
Marton ILLES <marton.illes@iot-inspector.com>
2022-02-19 23:26:35 +0100

    uninlined compressor_uncompress
    Needed more logic for compression auto-detection, no longer inlined
    
    calls the currently selected decompressor, unless that fails,
    then tries the other decompressors

--- squashfs-tools/compressor.c
+++ squashfs-tools/compressor.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include "compressor.h"
 #include "squashfs_fs.h"
+#include "error.h"
 
 #ifndef GZIP_SUPPORT
 static struct compressor gzip_comp_ops =  {
@@ -81,6 +82,19 @@ struct compressor *compressor[] = {
 };
 
 
+int lookup_compressor_index(char *name)
+{
+    int i;
+
+    for(i = 0; compressor[i]->id; i++)
+    {
+        if(strcmp(name, compressor[i]->name) == 0)
+            return i;
+    }
+
+    return -1;
+}
+
 struct compressor *lookup_compressor(char *name)
 {
 	int i;
@@ -134,4 +148,68 @@ void display_compressor_usage(char *def_comp)
 				fprintf(stderr, "\t%s (no options)%s\n",
 					compressor[i]->name, str);
 		}
+}
+
+// CJH: calls the currently selected decompressor, unless that fails, then tries the other decompressors
+int detected_compressor_index = 0;
+int compressor_uncompress(struct compressor *comp, void *dest, void *src, int size, int block_size, int *error)
+{
+    int i = 0, retval = -1, default_compressor_id = -1;
+
+    if(detected_compressor_index)
+    {
+        retval = compressor[detected_compressor_index]->uncompress(dest, src, size, block_size, error);
+    }
+
+    if(retval < 1 && comp->uncompress)
+    {
+        if(!detected_compressor_index) {
+          ERROR("Trying to decompress using default %s decompressor...\n", comp->name);
+        }
+
+        retval = comp->uncompress(dest, src, size, block_size, error);
+
+        if(!detected_compressor_index)
+        {
+            if(retval > 0)
+            {
+                ERROR("Successfully decompressed with default %s decompressor\n", comp->name);
+                detected_compressor_index = lookup_compressor_index(comp->name);
+            }
+            else
+            {
+                TRACE("Default %s decompressor failed! [%d %d]\n", comp->name, retval, *error);
+            }
+        }
+    }
+
+    if(retval < 1)
+    {
+        default_compressor_id = comp->id;
+
+        for(i=0; compressor[i]->id; i++)
+        {
+            comp = compressor[i];
+
+            if(comp->id != default_compressor_id &&
+               comp->id != compressor[detected_compressor_index]->id &&
+               comp->uncompress)
+            {
+                ERROR("Trying to decompress with %s...\n", comp->name);
+                retval = comp->uncompress(dest, src, size, block_size, error);
+                if(retval > 0)
+                {
+                    ERROR("Detected %s compression\n", comp->name);
+                    detected_compressor_index = i;
+                    break;
+                }
+                else
+                {
+                    TRACE("%s decompressor failed! [%d %d]\n", comp->name, retval, *error);
+                }
+            }
+        }
+    }
+
+    return retval;
 }
